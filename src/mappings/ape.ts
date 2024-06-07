7e826e8dbf6df8bf9808d3b6ff8eb520278aa80e
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-06-07 09:59:31 -0700

    feat: added fromUP

--- src/mappings/ape.ts
+++ src/mappings/ape.ts
@@ -5,11 +5,15 @@ import { Transfer } from "../../generated/templates/APE/APE";
 export function handleTransferFrom(event: Transfer): void {
   let context = dataSource.context();
   let apeAddress = context.getString("apeAddress");
-  const up = UserPosition.load(event.params.to.toHexString() + apeAddress);
-
-  if (up) {
-    up.balance = event.params.amount.plus(up.balance);
-    up.save();
+  const toUP = UserPosition.load(event.params.to.toHexString() + apeAddress);
+  const fromUP = UserPosition.load(
+    event.params.from.toHexString() + apeAddress
+  );
+  // TODO
+  // NEED TO CHECK FOR BURNING
+  if (toUP) {
+    toUP.balance = event.params.amount.plus(toUP.balance);
+    toUP.save();
   } else {
     const newUP = new UserPosition(event.params.to.toHexString() + apeAddress);
     newUP.User = event.params.to;
