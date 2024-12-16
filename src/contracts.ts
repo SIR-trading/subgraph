51c1745b74783c1fb8a21866c2f729367b04cad7
Tarp <travistarpley0@gmail.com>
2024-12-16 13:00:25 -0800

    feat: latest private addresses

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,3 +1,3 @@
-export const vaultAddress = "0x6712008CCD96751d586FdBa0DEf5495E0E22D904";
-export const sirAddress = "0xbB57FE325e769DEDB1236525a91cDEd842143fA7";
+export const vaultAddress = "0xD69BC314bdaa329EB18F36E4897D96A3A48C3eeF";
+export const sirAddress = "0x205Cfc23ef26922E116135500abb4B12Ab6d4668";
 export const zeroAddress = "0x0000000000000000000000000000000000000000";
