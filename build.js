fc8d15d8b4a9eb07d07b9c2b4c50c1cc6dcafaa6
Codonyat <nil.garcia@gmail.com>
2025-03-09 22:01:21 +0100

    the quoter and tokens addresses are filled up automatically for the user

--- build.js
+++ build.js
@@ -23,9 +23,15 @@ fs.writeFileSync(
 export const vaultAddress = "${process.env.VAULT_ADDRESS}";
 export const sirAddress = "${process.env.SIR_ADDRESS}";
 export const zeroAddress = "0x0000000000000000000000000000000000000000";
-export const quoterAddress = "${process.env.QUOTER_ADDRESS}";
-export const usdcAddress = "${process.env.USDC_ADDRESS}";
-export const wethAddress = "${process.env.WETH_ADDRESS}";
+export const quoterAddress = "${process.env.NETWORK === "mainnet" ? 
+  "0x5e55c9e631fae526cd4b0526c4818d6e0a9ef0e3" : process.env.NETWORK === "sepolia" ?
+  "0xe3c07ebF66b9D070b589bCCa30903891F71A92Be" : process.env.QUOTER_ADDRESS }";
+export const usdcAddress = "${process.env.NETWORK === "mainnet" ? 
+  "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" : process.env.NETWORK === "sepolia" ?
+  "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238" : process.env.USDC_ADDRESS }";
+export const wethAddress = "${process.env.NETWORK === "mainnet" ? 
+  "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" : process.env.NETWORK === "sepolia" ?
+  "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14" : process.env.WETH_ADDRESS }";
 `,
 );
 console.log("src/contracts.ts and subgraph.yaml generated successfully.");
--- src/contracts.ts
+++ src/contracts.ts
@@ -1,6 +1,7 @@
-export const vaultAddress = "0xB91AE2c8365FD45030abA84a4666C4dB074E53E7";
-export const sirAddress = "0x1278B112943Abc025a0DF081Ee42369414c3A834";
+
+export const vaultAddress = "0x8f52748dfE1D8c90b7C6d8a0d15A552015e8Dd4E";
+export const sirAddress = "0x8001f42274d2821391719225Ee0352E624590dB3";
 export const zeroAddress = "0x0000000000000000000000000000000000000000";
-export const quoterAddress = "0x5e55c9e631fae526cd4b0526c4818d6e0a9ef0e3";
-export const usdcAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-export const wethAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+export const quoterAddress = "0xe3c07ebF66b9D070b589bCCa30903891F71A92Be";
+export const usdcAddress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";
+export const wethAddress = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14";
